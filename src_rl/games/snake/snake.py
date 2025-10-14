ce23ea0d0d30b5f0c6cd294291ca805cc2edae0c
mnozkiewicz <michno513@gmail.com>
2025-10-14 23:52:20 +0200

    Implemented snake

--- src_rl/games/snake/snake.py
+++ src_rl/games/snake/snake.py
@@ -1,109 +1,61 @@
-# from collections import deque
-# from dataclasses import dataclass
-# from enum import Enum
-# import numpy as np
-# from typing import Optional
+from collections import deque
+import numpy as np
+from typing import Optional
+from .utils import Pos, Dir
+from numpy.typing import NDArray
 
 
-# @dataclass(frozen=True, eq=True)
-# class Pos:
-#     x: int
-#     y: int
+class Snake:
+    def __init__(self, x: int, y: int, board_size: int):
+        self._head: Pos = Pos(x, y)
+        self._tail: deque[Pos] = deque([self._head])
+        self._last_pos: Pos = Pos(x, y)
+        self._board_size: int = board_size
 
-#     def __add__(self, other: 'Pos') -> 'Pos':
-#         return Pos(self.x + other.x, self.y + other.y)
+        self._board: NDArray[np.bool] = np.full((board_size, board_size), 0)
+        self._board[x, y] = 1
 
-#     def mod_index(self, size: int) -> 'Pos':
-#         return Pos(self.x % size, self.y % size)
+        self._dir = Dir.UP
 
-#     def __iter__(self):
-#         yield self.x
-#         yield self.y
+    def head(self) -> Pos:
+        return self._head
 
+    def dir(self) -> Dir:
+        return self._dir
 
-# class Dir(Enum):
-#     UP: str = 'UP'
-#     DOWN: str = 'DOWN'
-#     LEFT: str = 'LEFT'
-#     RIGHT: str = 'RIGHT'
+    def length(self) -> int:
+        return len(self._tail) + 1
 
-#     def vector(self) -> Pos:
-#         match self:
-#             case Dir.UP:
-#                 return Pos(0, -1)
-#             case Dir.DOWN:
-#                 return Pos(0, 1)
-#             case Dir.LEFT:
-#                 return Pos(-1, 0)
-#             case Dir.RIGHT:
-#                 return Pos(1, 0)
-#             case _:
-#                 pass
+    def tail(self) -> list[Pos]:
+        return list(self._tail)
 
-#     def opposite(self) -> 'Dir':
-#         match self:
-#             case Dir.UP:
-#                 return Dir.DOWN
-#             case Dir.DOWN:
-#                 return Dir.UP
-#             case Dir.LEFT:
-#                 return Dir.RIGHT
-#             case Dir.RIGHT:
-#                 return Dir.LEFT
+    def board(self) -> np.ndarray:
+        return self._board
 
-# class Snake:
-#     def __init__(self, x: int, y: int, board_size: int):
+    def move(self) -> None:
+        next_pos = (self._head + self._dir.vector()).mod_index(self._board_size)
+        self._head = next_pos
 
-#         self._head: Pos = Pos(x, y)
-#         self._tail: deque[Pos] = deque([self._head])
-#         self._last_pos: Pos = Pos(x, y)
-#         self._board_size: int = board_size
+        # Add new position
+        self._tail.appendleft(self._head)
+        self._board[*self._head] += 1
 
-#         self._board: np.ndarray[np.bool] = np.full((board_size, board_size), 0)
-#         self._board[x, y] = 1
+        # Remove last position
+        self._last_pos = self._tail.pop()
+        self._board[*self._last_pos] -= 1
 
-#         self._dir = Dir.UP
+    def turn(self, dir: Optional[Dir]) -> None:
+        if dir is not None:
+            if self._dir.opposite() == dir:
+                return
+            self._dir = dir
 
-#     def head(self) -> Pos:
-#         return self._head
+    def eat_food(self, food: Pos) -> bool:
+        if food == self._head:
+            self._tail.append(self._last_pos)
+            self._board[*self._last_pos] += 1
+            return True
+        return False
 
-#     def dir(self) -> Dir:
-#         return self._dir
-
-#     def length(self) -> int:
-#         return len(self._tail) + 1
-
-#     def tail(self) -> list[Pos]:
-#         return list(self._tail)
-
-#     def board(self) -> np.ndarray:
-#         return self._board
-
-#     def move(self) -> None:
-#         next_pos = (self._head + self._dir.vector()).mod_index(self._board_size)
-#         self._head = next_pos
-
-#         # Add new position
-#         self._tail.appendleft(self._head)
-#         self._board[*self._head] += 1
-
-#         # Remove last position
-#         self._last_pos = self._tail.pop()
-#         self._board[*self._last_pos] -= 1
-
-#     def turn(self, dir: Optional[Dir]) -> None:
-#         if dir is not None:
-#             if self._dir.opposite() == dir:
-#                 return
-#             self._dir = dir
-
-#     def eat_food(self, food: Pos) -> bool:
-#         if food == self._head:
-#             self._tail.append(self._last_pos)
-#             self._board[*self._last_pos] += 1
-#             return True
-#         return False
-
-
-#     def collision(self) -> bool:
-#         return np.any(self._board.max() >= 2)
+    def collision(self) -> np.bool:
+        return np.any(self._board.max() >= 2)
---
+++ src_rl/games/snake/utils.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+import numpy as np
+from numpy.typing import NDArray
+
+
+@dataclass(frozen=True, eq=True)
+class Pos:
+    x: int
+    y: int
+
+    def __add__(self, other: Pos) -> Pos:
+        return Pos(self.x + other.x, self.y + other.y)
+
+    def mod_index(self, size: int) -> Pos:
+        return Pos(self.x % size, self.y % size)
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+
+class Dir(Enum):
+    UP = "UP"
+    DOWN = "DOWN"
+    LEFT = "LEFT"
+    RIGHT = "RIGHT"
+
+    def vector(self) -> Pos:
+        match self:
+            case Dir.UP:
+                return Pos(0, -1)
+            case Dir.DOWN:
+                return Pos(0, 1)
+            case Dir.LEFT:
+                return Pos(-1, 0)
+            case Dir.RIGHT:
+                return Pos(1, 0)
+
+    def opposite(self) -> Dir:
+        match self:
+            case Dir.UP:
+                return Dir.DOWN
+            case Dir.DOWN:
+                return Dir.UP
+            case Dir.LEFT:
+                return Dir.RIGHT
+            case Dir.RIGHT:
+                return Dir.LEFT
+
+
+class Action(Enum):
+    QUIT = 0
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+    NOTHING = 5
+
+    def to_dir(self) -> Optional[Dir]:
+        match self:
+            case Action.UP:
+                return Dir.UP
+            case Action.DOWN:
+                return Dir.DOWN
+            case Action.LEFT:
+                return Dir.LEFT
+            case Action.RIGHT:
+                return Dir.RIGHT
+            case _:
+                return None
+
+    @staticmethod
+    def move_actions() -> list["Action"]:
+        return [Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT]
+
+
+@dataclass
+class State:
+    board_size: int
+    head: Pos
+    tail: list[Pos]
+    food: Pos
+    snake_dir: Dir
+    board: NDArray[np.int_]
+    running: bool = True
